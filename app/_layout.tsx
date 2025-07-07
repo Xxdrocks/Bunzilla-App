6215ce4b6f8caf2a439bc9019e3bbbeaae02f81f
Xxdrocks <kerjaanpahri@gmail.com>
2025-07-06 23:17:36 -0700

    nativewind setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [] = useFonts({
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Reguler": require("../assets/fonts/Quicksand-Reguler.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+ useEffect(() => {
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  useEffect(() => {
+    fetchAuthenticatedUser()
+  }, []);
+
+  if(!fontsLoaded || isLoading) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
