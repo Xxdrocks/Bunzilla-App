67f76c308c87058b11f8fd2ac78726554da36ad6
Xxdrocks <kerjaanpahri@gmail.com>
2025-07-12 21:16:02 -0700

    app update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
+import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-between flex-row w-full my-5 px-5 ">
-        <View className="flex-start">
-          <Text className="small-bold text-primary">DELIVER TO</Text>
-        </View>
-      </View>
-
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
@@ -60,6 +62,22 @@ export default function Index() {
           );
         }}
         contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5 ">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-2 ">
+                <Text className="paragraph-bold text-dark-100">Indonesia</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
