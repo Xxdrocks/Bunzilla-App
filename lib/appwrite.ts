7fa99f5dcb9f5c201963aa1a5417589d253a327d
Xxdrocks <kerjaanpahri@gmail.com>
2025-07-13 20:47:28 -0700

    appwrite commit

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+
+export const appwriteConfig = {
+    endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    Platform : "com.winata.bunzilla",
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId : '687428e80023a5e70036',
+    userId : '6874294c0008b1e4fda5',
+    
+}
